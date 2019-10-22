f247f9966a471d1fe19cc9db491925a6c6de2c84
Syava7 <syava77787@gmail.com>
2019-10-22 17:26:21 +0300

    updates

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -1,13 +1,37 @@
 import React from 'react'
-// import classes from './MyPosts.module.css'
+import classes from './MyPosts.module.css'
 import Post from './Post/Post'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
 const MyPosts = () => {
+
+  const addPost = (changeInput) => {
+    
+    
+    
+  }
+
+  const changeInput = (e) => {
+    const text = e.target.value
+    console.log(text)
+  }
+ 
   return (
     <div>
-      My posts
+      <h3>My posts</h3>
       <div>
-        
+        <div>
+          <TextField label="Name" onChange={ changeInput } value='syava'/>
+        </div>
+        <div className={classes.button}>
+          <Button
+              onClick={ addPost } 
+              variant="contained" 
+              color="primary" >
+            Add post
+          </Button>
+        </div>
       </div>
       <div>
         <Post message='Hello guys' likes='15'/>
