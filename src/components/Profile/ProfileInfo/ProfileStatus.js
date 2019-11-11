b655a1396b1d99fea0b4bb0c916eb5c5f95d441e
Syava7 <syava77787@gmail.com>
2019-11-11 17:35:28 +0200

    changes

--- src/components/Profile/ProfileInfo/ProfileStatus.js
+++ src/components/Profile/ProfileInfo/ProfileStatus.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Input from '@material-ui/core/Input'
 
 const ProfileStatus = (props) => {
 
   const [editMode, setEditMode] = useState(false)
   const [status, setStatus] = useState(props.status)
+
+  useEffect( () => {
+    setStatus(props.status)
+  }, [props.status] )
 
   const activateEditMode = () => {
     setEditMode(true)
