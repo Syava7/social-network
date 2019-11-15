5a518fb50d8a9050407d5f276176cf1ef12b0cbf
Syava7 <syava77787@gmail.com>
2019-11-15 15:06:12 +0200

    changes

--- src/components/common/Paginator/Paginator.js
+++ src/components/common/Paginator/Paginator.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from '@material-ui/core/Button'
 import classes from './Paginator.module.css'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
@@ -6,20 +6,32 @@ import ButtonGroup from '@material-ui/core/ButtonGroup'
 
 
 
-const Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged}) => {
+
+const Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
 
   let pagesCount = Math.ceil(totalUsersCount / pageSize)
+  
 
   let pages = []
   for (let i = 1; i < pagesCount; i++) {
     pages.push(i)
   }
 
+  let portionCount = Math.ceil(pagesCount / portionSize)
+  let [portionNumber, setPortionNumber] = useState(1)
+  let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+  let rightPortionPageNumber = portionNumber * portionSize
+
+
   return (
       <div>
+        {portionNumber > 1 &&
+        <button onClick={ ()=> {setPortionNumber(portionNumber - 1)}}>prev</button>}
         <ButtonGroup>
           {
-            pages.map(page => {
+            pages
+            .filter(page => page >= leftPortionPageNumber && page <= rightPortionPageNumber)
+            .map(page => {
               return <Button className={currentPage === page && classes.selectedPage}
                               onClick={ (e) => {
                               onPageChanged(page)
@@ -29,6 +41,8 @@ const Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged}) => {
             })
           }
         </ButtonGroup>
+        {portionCount > portionNumber &&
+        <button onClick={ ()=> {setPortionNumber(portionNumber + 1)}}>next</button>}
       </div>
   
   )
